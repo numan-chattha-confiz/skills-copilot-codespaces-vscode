1b5063c70b822f786f06268e42aa7435a44039d4
Numan Chattha <numan.chattha@walmart.com>
2024-04-25 22:52:01 -0500

    add member.js

---
+++ member.js
@@ -0,0 +1,23 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 30,
+        skills: ["JavaScript", "React", "Node"],
+        details: {
+            id: 101,
+            email: ""
+        }   
+    }
+}
+
+function skillsMember2() {
+    var member = {
+        name: "John Doe",
+        age: 30,
+        skills: ["JavaScript", "React", "Node"],
+        details: {
+            id: 101,
+            email: ""
+        }   
+    }
+}
