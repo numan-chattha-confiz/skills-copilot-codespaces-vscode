5aa4a4605ac0bce9cfcec4bc9124043e7aadd3b0
Numan Chattha <numan.chattha@walmart.com>
2024-04-27 17:47:34 -0500

    no message

---
+++ comments.js
@@ -0,0 +1,35 @@
+//create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const commentsPath = path.join(__dirname, 'comments.json');
+const comments = require(commentsPath);
+
+//middleware
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+//get all comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+//add a comment
+app.post('/comments', (req, res) => {
+  const comment = req.body;
+  comments.push(comment);
+  fs.writeFile(commentsPath, JSON.stringify(comments), (err) => {
+    if (err) {
+      res.status(500).send('An error occurred');
+    } else {
+      res.status(201).send('Comment added');
+    }
+  });
+});
+
+//start server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
--- member.js
+++ member.js
@@ -21,3 +21,4 @@ function skillsMember2() {
         }   
     }
 }
+    
