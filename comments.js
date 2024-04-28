383a20d6b9f90be725ce2e1f5d9e5fcaa6c23421
Numan Chattha <numan.chattha@walmart.com>
2024-04-27 19:49:16 -0500

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,37 +1,14 @@
 //create web server
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
-const commentsPath = path.join(__dirname, 'comments.json');
-const comments = require(commentsPath);
+var http = require('http');
+var fs = require('fs');
 
-//middleware
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-//get all comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-//add a comment
-app.post('/comments', (req, res) => {
-  const comment = req.body;
-  comments.push(comment);
-  fs.writeFile(commentsPath, JSON.stringify(comments), (err) => {
-    if (err) {
-      res.status(500).send('An error occurred. Please try again later.');
-      console.error('Error writing to comments.json file:', err);
-      console.error('Error writing to comments.json file:', err);
-    } else {
-      res.status(201).send('Comment added');
-    }
+//create server
+http.createServer(function(req, res){
+  //read file
+  fs.readFile('index.html', function(err, data){
+    //write response
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
   });
-});
-
-//start server
-//app.listen(3000, () => {
-  //console.log('Server is running on port 3000');
-//});
+}).listen(8080);
