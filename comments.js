2f201e517c75ad2ea64f4775f7a56ee3f6aee6a1
Numan Chattha <numan.chattha@walmart.com>
2024-04-27 19:57:30 -0500

    kkk

--- comments.js
+++ comments.js
@@ -1,14 +1,40 @@
 //create web server
-var http = require('http');
-var fs = require('fs');
-
-//create server
-http.createServer(function(req, res){
-  //read file
-  fs.readFile('index.html', function(err, data){
-    //write response
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const comments = [];
+const server = http.createServer((req, res) => {
+    const { pathname, query } = url.parse(req.url, true);
+    if (pathname === '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
+            if (err) {
+                return res.end('404 Not Found');
+            }
+            res.end(data);
+        });
+    } else if (pathname === '/comments') {
+        if (req.method === 'GET') {
+            res.end(JSON.stringify(comments));
+        } else if (req.method === 'POST') {
+            let str = '';
+            req.on('data', chunk => {
+                str += chunk;
+            });
+            req.on('end', () => {
+                comments.push(JSON.parse(str));
+                res.end(JSON.stringify(comments));
+            });
+        }
+    } else {
+        fs.readFile(path.join(__dirname, pathname), 'utf8', (err, data) => {
+            if (err) {
+                return res.end('404 Not Found');
+            }
+            res.end(data);
+        });
+    }
+});
+server.listen(3000, () => {
+    console.log('Server running at http://'); // Fix: Added closing quotation mark
+});
