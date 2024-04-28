537ad005337602c52921b4a410385cb41ab5ecec
Numan Chattha <numan.chattha@walmart.com>
2024-04-27 20:08:32 -0500

    lll

--- comments.js
+++ comments.js
@@ -1,40 +1,77 @@
-//create web server
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
-const comments = [];
-const server = http.createServer((req, res) => {
-    const { pathname, query } = url.parse(req.url, true);
-    if (pathname === '/') {
-        fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
-    } else if (pathname === '/comments') {
-        if (req.method === 'GET') {
-            res.end(JSON.stringify(comments));
-        } else if (req.method === 'POST') {
-            let str = '';
-            req.on('data', chunk => {
-                str += chunk;
-            });
-            req.on('end', () => {
-                comments.push(JSON.parse(str));
-                res.end(JSON.stringify(comments));
+// create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+//parse body of post request
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//get comments from json file
+app.get('/comments', function(req, res){
+    fs.readFile('comments.json', 'utf8', function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(data);
+        }
+    });
+});
+
+//add comments to json file
+app.post('/comments', function(req, res){
+    fs.readFile('comments.json', 'utf8', function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var comments = JSON.parse(data);
+            comments.push(req.body);
+            fs.writeFile('comments.json', JSON.stringify(comments), function(err){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.send('Comment added');
+                }
             });
         }
+    });
+});
+
+//start server
+app.listen(3000, function(){
+    console.log('Server is running on port 3000');
+});
+//create a variable to store the express module
+//require the express module
+var express = require('express');
+//create an instance of the express module
+var app = express();
+//require the body-parser module
+var bodyParser = require('body-parser');
+//parse the request body
+app.use(bodyParser.json());
+//require the mongodb module
+var mongodb = require('mongodb');
+//create a variable to store the MongoClient
+var MongoClient = mongodb.MongoClient;
+//create a variable to store the url of the database
+var url = 'mongodb://localhost:27017/comments';
+//create a variable to store the collection of the database
+var collection;
+//connect to the database
+MongoClient.connect(url, function(err, db){
+    if(err){
+        console.log('Unable to connect to the Server', err);
     } else {
-        fs.readFile(path.join(__dirname, pathname), 'utf8', (err, data) => {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            res.end(data);
-        });
+        console.log('Connected to Server');
+        collection = db.collection('comments');
     }
 });
-server.listen(3000, () => {
-    console.log('Server running at http://'); // Fix: Added closing quotation mark
-});
+//create a variable to store the port number
+var port = 3000;
+//create a variable to store the ip address
+var ip = '127.0.0.1';
