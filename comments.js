b67a586e613d391d48d00504fe810b07522895a0
Numan Chattha <numan.chattha@walmart.com>
2024-04-27 20:11:28 -0500

    ooo

--- comments.js
+++ comments.js
@@ -1,77 +1,23 @@
-// create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
+// Create web server
+// 1. Create a web server
+// 2. Read the comments.json file
+// 3. Respond to the client with the contents of the comments.json file
+// 4. Listen on port 3000
 
-//parse body of post request
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// 1. Create a web server
+const http = require('http');
+const fs = require('fs');
 
-//get comments from json file
-app.get('/comments', function(req, res){
-    fs.readFile('comments.json', 'utf8', function(err, data){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.send(data);
-        }
-    });
+const server = http.createServer((req, res) => {
+  // 2. Read the comments.json file
+  fs.readFile('./comments.json', 'utf8', (err, data) => {
+    // 3. Respond to the client with the contents of the comments.json file
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(data);
+  });
 });
 
-//add comments to json file
-app.post('/comments', function(req, res){
-    fs.readFile('comments.json', 'utf8', function(err, data){
-        if(err){
-            console.log(err);
-        }
-        else{
-            var comments = JSON.parse(data);
-            comments.push(req.body);
-            fs.writeFile('comments.json', JSON.stringify(comments), function(err){
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    res.send('Comment added');
-                }
-            });
-        }
-    });
+// 4. Listen on port 3000
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
-
-//start server
-app.listen(3000, function(){
-    console.log('Server is running on port 3000');
-});
-//create a variable to store the express module
-//require the express module
-var express = require('express');
-//create an instance of the express module
-var app = express();
-//require the body-parser module
-var bodyParser = require('body-parser');
-//parse the request body
-app.use(bodyParser.json());
-//require the mongodb module
-var mongodb = require('mongodb');
-//create a variable to store the MongoClient
-var MongoClient = mongodb.MongoClient;
-//create a variable to store the url of the database
-var url = 'mongodb://localhost:27017/comments';
-//create a variable to store the collection of the database
-var collection;
-//connect to the database
-MongoClient.connect(url, function(err, db){
-    if(err){
-        console.log('Unable to connect to the Server', err);
-    } else {
-        console.log('Connected to Server');
-        collection = db.collection('comments');
-    }
-});
-//create a variable to store the port number
-var port = 3000;
-//create a variable to store the ip address
-var ip = '127.0.0.1';
